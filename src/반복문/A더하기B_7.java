644d3678f90802212b47ce66803af8adc4d0cf67
JungMin <ilb0919497@gmail.com>
2023-08-27 20:14:03 +0900

    - 문제 추가

---
+++ src/반복문/A더하기B_7.java
@@ -0,0 +1,29 @@
+package 반복문;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/*
+https://www.acmicpc.net/problem/11021
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ */
+public class A더하기B_7 {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int caseCount = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= caseCount; i++){
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            bw.write("Case #" + i + ": " + (num1 + num2) + "\n");
+
+        }
+        bw.close();
+    }
+}
